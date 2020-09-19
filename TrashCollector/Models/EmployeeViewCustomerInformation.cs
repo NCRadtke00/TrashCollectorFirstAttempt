93287d7c1a9ded936bd911615509f9bcfa15ca9d
NCRadtke00 <raddnc@gmail.com>
2020-09-19 14:39:13 -0500

    added a model for employee to view certain customer information. in the class the employee will be able to see which customers are suppose to have their trash collected on which day and if the trash was collected

---
+++ TrashCollector/Models/EmployeeViewCustomerInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class EmployeeViewCustomerInformation
+    {
+        public Employee employee { get; set; } 
+        public List<Customer>  customers { get; set; }
+        public string PickADay { get; set; }
+        public List<Customer> dayPicked { get; set; }
+        public bool wasTrashPickedUp { get; set; }
+
+
+    }
+}
