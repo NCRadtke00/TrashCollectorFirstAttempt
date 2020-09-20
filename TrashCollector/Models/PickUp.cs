f7b1a48a124b1511d8006aad761f78e22c8257aa
NCRadtke00 <raddnc@gmail.com>
2020-09-20 17:28:31 -0500

    changed emplyoeeroute to pickup

--- TrashCollector/Models/PickUp.cs
+++ TrashCollector/Models/PickUp.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,17 +9,8 @@ namespace TrashCollector.Models
     public class PickUp
     {
         [Key]
-        public int pickUpId { get; set; }
-        [ForeignKey ("employee")]
-        public int employeeId { get; set; }
         public Employee employee { get; set; } 
-        [ForeignKey("customers")] 
-        public int customerId { get; set; }
-        public Customer customers { get; set; }
-      
-        public DateTime PickUpDate { get; set; }
-        public bool employeeCollectedTrash { get; set; }
-        public bool employeeCollectedRecycling { get; set; }
+        public List<Customer> customers { get; set; }
         public bool customerTrashWasNotCollected { get; set; }
         public bool customerRecyclingWasNotCollected { get; set; }
     }
