fb6e89d5c3fa5a8df709a0e80c3c62b0a71256c9
Nicholas Radtke <Raddnc@gmail.com>
2020-11-02 12:46:36 -0600

    creating services model

--- TrashCollector/Models/Services.cs
+++ TrashCollector/Models/Services.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +10,10 @@ namespace TrashCollector.Models
 {
     public class Services
     {
-
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Active Account" )]
+        [DefaultValue(true)]
+        public bool IsAccActive { get; set; }
     }
 }
