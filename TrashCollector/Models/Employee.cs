7e1fbe121a6bb6478d12d812b7c53ec53ab2e188
Nicholas Radtke <Raddnc@gmail.com>
2020-11-02 15:23:42 -0600

    made light changes to emp model

--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -14,18 +14,7 @@ namespace TrashCollector.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string PhosneNumber { get; set; }
-        [ForeignKey("Address")]
-        public int AddressId { get; set; }
-        public Address Address { get; set; }
-        public string Street { get; set; }
-        public string ApartmantOrSuiteNumber { get; set; }
-        public string CityName { get; set; }
-        public string StateName { get; set; }
         public string ZipCode { get; set; }
-        public string Cordinates { get; set; }
-
-
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
         public IdentityUser IdentityUser { get; set; }
---
+++ TrashCollector/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class EmployeeViewModel
+    {
+        public Employee Employee { get; set; }
+        public Customer Customer { get; set; }
+        public List<Customer> Customers { get; set; }
+        public DayOfWeek?  QueryDay { get; set; }
+    }
+}
